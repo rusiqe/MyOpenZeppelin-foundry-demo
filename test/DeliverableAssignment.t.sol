f35860dbba58c7aaf0870022bd8e5d5e66830954
Taurai. M <rusiqe@gmail.com>
2023-06-26 16:51:37 +0200

    ran tests for delassign.t.sol

---
+++ test/DeliverableAssignment.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/DeliverableAssignment.sol";
+
+contract FoundryDemoTest is Test {
+  DeliverableAssignment instance;
+  uint256 internal testNumber = 27;
+  address internal testAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+function setUp() public {
+    instance = new DeliverableAssignment();
+  }
+
+function test_SecretNumber() public {
+    assertEq(testNumber, instance.secretNumber());
+  }
+
+  function testFail_Owner() public {
+    assertEq(testAddress, instance.owner());
+  }
+}
